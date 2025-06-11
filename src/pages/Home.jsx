63bc4c1fd164c3b0b28f69a188e719257a17d90d
jtweedle1 <jazminrtweedle@gmail.com>
2025-06-11 12:46:27 -0500

    Change title and favicon, style home titles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import SignUpForm from "../components/Home/SignUpForm";
 import LoginForm from "../components/Home/LoginForm";
+import "./Home.css";
 
 export default function Home() {
   return (
     <>
+      <div className="home-titles">
+        <h1 className="brainboy-title">BrainBoy</h1>
+        <h1 className="advance-title">Advance</h1>
+      </div>
       <SignUpForm />
       <LoginForm />
     </>
